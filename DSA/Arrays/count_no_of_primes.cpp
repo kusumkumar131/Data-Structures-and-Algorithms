feb7eafb87b7faa510c6224d9a232c8b9833fbc2
Kusum <kusumkumaryendredla@gmail.com>
2026-06-26 22:05:38 +0530

    Add Merge Sort and Count Primes

---
+++ DSA/Arrays/count_no_of_primes.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    //sieve of eratosthenes
+    int count=0;
+    bool isPrime[n+1];  
+    for(int i=0;i<=n;i++){
+        isPrime[i]=true;
+    }
+    //write code
+    for(int i=2;i*i<=n;i++){
+        if(isPrime[i]){
+            for(int j=i*i;j<=n;j+=i){
+                isPrime[j]=false;
+            }
+        }
+    }
+
+
+    return 0;
+}
