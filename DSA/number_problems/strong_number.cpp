b7ea14e256b7927351e6d1fde733bdd30f530082
Kusum <kusumkumaryendredla@gmail.com>
2026-06-27 23:01:13 +0530

    strong number

---
+++ DSA/number_problems/strong_number.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int i=0,j=n-1;
+    while(i<j){ 
+        
+        if(arr[i]==0 && arr[j]!=0){
+            swap(arr[i],arr[j]);
+            i++;
+            j--;
+        }
+        else if(arr[i]!=0){
+            i++;
+        }
+        else if(arr[j]==0){
+            j--;
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
