d9f4a766061f9c92a0d600d6cf69caa44e2586b9
Kusum <kusumkumaryendredla@gmail.com>
2026-06-28 22:16:50 +0530

    binary_search

---
+++ DSA/sorting/Binary_search.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int n, int target) {
+    int left = 0;
+    int right = n - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        }
+
+        if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    int arr[] = {2, 3, 7, 7, 11, 15, 25}; // Must be sorted
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 11;
+
+    int result = binarySearch(arr, n, target);
+
+    if (result != -1) {
+        cout << "Element found at index: " << result << endl;
+    } else {
+        cout << "Element not found" << endl;
+    }
+
+    return 0;
+}
