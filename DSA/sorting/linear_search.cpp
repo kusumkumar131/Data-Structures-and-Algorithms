dc7d0473a9c76859f8a3edd9bb4dc8adadc3b37d
Kusum <kusumkumaryendredla@gmail.com>
2026-06-28 22:15:46 +0530

    linear_search

---
+++ DSA/sorting/linear_search.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int linearSearch(int arr[], int n, int target) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {12, 8, 9, 11, 5, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 11;
+
+    int result = linearSearch(arr, n, target);
+
+    if (result != -1) {
+        cout << "Element found at index: " << result << endl;
+    } else {
+        cout << "Element not found" << endl;
+    }
+
+    return 0;
+}
