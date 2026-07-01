947b53ffb0fb489f40e1ce60bf5e8a88659fa964
Kusum <kusumkumaryendredla@gmail.com>
2026-07-01 22:56:20 +0530

    singly

---
+++ DSA/Linked_List/singly_linkedlist.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+struct Node{
+    int data;
+    Node* next;
+    Node(int val){
+        data=val;
+        next=NULL;
+    }
+};
+
+int main(){
+        Node* head=new Node(10);
+        int n;
+        cin>>n;
+        vector<int> arr;
+        for(int i=0;i<n;i++){
+            int x;
+            cin>>x;
+            arr.push_back(x);
+        }   
+        while(!arr.empty()){
+            Node* temp=new Node(arr.back());
+            arr.pop_back();
+            temp->next=head;
+            head=temp;
+        }
+
+}
