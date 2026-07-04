a79b3cf510c4af5e326c19accccd675947cb9633
Kusum <kusumkumaryendredla@gmail.com>
2026-07-04 23:06:44 +0530

    push

---
+++ DSA/Math/perimeter_of_rectangle.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int length,breadth;
+    cin>>length>>breadth;
+    int perimeter=2*(length+breadth);
+    cout<<perimeter;
+    return 0;
+}
