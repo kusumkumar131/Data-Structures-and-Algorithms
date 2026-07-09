93fa581828354249672d3e546d45e941fb9168dd
Kusum <kusumkumaryendredla@gmail.com>
2026-07-09 23:10:09 +0530

    single_element_in_a_sorted_array

---
+++ DSA/BInary_search/single_element _in_a_sorted_array.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    // Binary search for the single element
+    int low = 0, high = n - 1;
+    while (low < high) {
+        int mid = low + (high - low) / 2;
+        if (mid % 2 == 0) {
+            if (arr[mid] == arr[mid + 1]) {
+                low = mid + 2;
+            } else {
+                high = mid;
+            }
+        } else {
+            if (arr[mid] == arr[mid - 1]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+    }
+    cout << arr[low] << endl;
+
+    return 0;
+}
