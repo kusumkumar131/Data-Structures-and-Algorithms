4e1e37e488855dea7ad2461e7f18c7b2f042e699
Kusum <kusumkumaryendredla@gmail.com>
2026-07-14 23:55:02 +0530

    x

--- Python/tuple.py
+++ Python/tuple.py
@@ -2,3 +2,10 @@ l=tuple([1,23,'k','kusum'])
 print(l)
 thistuple = ("apple", "banana", "cherry")
 print(thistuple[1])
+
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+
+print(x)
