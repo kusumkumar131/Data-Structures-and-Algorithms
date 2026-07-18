0ef4dd4655a91b038a7764a88fcecdb363cdf15e
Kusum <kusumkumaryendredla@gmail.com>
2026-07-18 23:11:03 +0530

    view

--- Python/list_comprehension.py
+++ Python/list_comprehension.py
@@ -14,3 +14,16 @@ print(m)
 
 for i in p:
     print(i**2)
+
+while True:
+    try:
+        n=int(input("Enter a number to check if it is in the list: "))
+        if n in p:
+            print(f"{n} is present in the list.")
+        else:
+            print(f"{n} is not present in the list.")
+    except ValueError:
+        print("Please enter a valid integer.")
+    except KeyboardInterrupt:
+        print("\nExiting the program.")
+        break
