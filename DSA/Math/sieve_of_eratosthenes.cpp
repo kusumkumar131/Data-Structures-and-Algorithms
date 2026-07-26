6cd28b86cab9a71d6de5f94bad0926457497fb7b
Kusum <kusumkumaryendredla@gmail.com>
2026-07-26 23:43:56 +0530

    sieve of eratosthenes

---
+++ DSA/Math/sieve_of_eratosthenes.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+
+    bool prime[n+1];
+    for(int i=0;i<=n;i++){
+        prime[i]=true;
+    }
+    prime[0]=false;
+    prime[1]=false;
+
+    for(int i=2;i*i<=n;i++){
+        if(prime[i]==true){
+            for(int j=i*i;j<=n;j+=i){
+                prime[j]=false;
+            }
+        }
+    }
+
+    cout<<"Prime numbers up to "<<n<<": ";
+    for(int i=2;i<=n;i++){
+        if(prime[i]==true){
+            cout<<i<<" ";
+        }
+    }
+    cout<<endl;
+    return 0;
+}
