db4b72cf950a5d3c8c9ca4a448bd43f8d8cc98ac
Kusum <kusumkumaryendredla@gmail.com>
2026-08-01 22:22:09 +0530

    sum of n natural numbers

--- DSA/Arrays/Missing_numer.cpp
+++ DSA/Arrays/Missing_numer.cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    int arr[n];
+    int arr[n];  //array of size n
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    int sum=(n*(n+1))/2;
+    int sum=(n*(n+1))/2; //sum of first n natural numbers
     for(int i=0;i<n;i++){
         sum-=arr[i];
     }
