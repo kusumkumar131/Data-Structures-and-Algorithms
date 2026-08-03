7b6a52af1244e1c54c53d6c6ade6f49088740807
Kusum <kusumkumaryendredla@gmail.com>
2026-08-03 23:57:19 +0530

    prefix

---
+++ DSA/Arrays/prefix_sum.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the size of the array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements of the array: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int prefix_sum[n];
+    prefix_sum[0]=arr[0];
+    for(int i=1;i<n;i++){
+        prefix_sum[i]=prefix_sum[i-1]+arr[i];
+    }
+    cout<<"The prefix sum array is: ";
+    for(int i=0;i<n;i++){
+        cout<<prefix_sum[i]<<" ";
+    }
+    return 0;
+}
