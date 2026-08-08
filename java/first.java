434ede5b8c4ef081ae5f29bb8760cc66576185c4
Kusum <kusumkumaryendredla@gmail.com>
2026-08-08 23:24:36 +0530

    add

--- java/first.java
+++ java/first.java
@@ -1,5 +1,10 @@
-class first{
-    public static void main(String[] args){
-        System.out.println("Hello World");
-    }
-}
+import java.util.*; 
+class first{ 
+    public static void main(String[] args){ 
+        System.out.println("Hello World"); 
+        int a = 10; 
+        Scanner sc = new Scanner(System.in); 
+        int p=sc.nextInt(); 
+        System.out.println("The value of p is: " + p); 
+    } 
+}   
