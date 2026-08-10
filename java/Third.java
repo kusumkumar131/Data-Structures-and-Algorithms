3376bd806ac00a7db561725f9a9e248f7651a00f
Kusum <kusumkumaryendredla@gmail.com>
2026-08-10 23:57:06 +0530

    third

---
+++ java/Third.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Third {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int a = 10;
+        Scanner sc = new Scanner(System.in);
+        int p = sc.nextInt();
+        System.out.println("The value of p is: " + p);
+        char ch = sc.next().charAt(0);
+        System.out.println("The value of ch is: " + ch);
+    }
+}
