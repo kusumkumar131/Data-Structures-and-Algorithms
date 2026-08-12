8e1066e3cff35aafd311ccc0e3f56cf376cbecd4
Kusum <kusumkumaryendredla@gmail.com>
2026-08-12 23:59:41 +0530

    push

---
+++ java/basics.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class basics {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int a ;
+    Scanner sc=new Scanner(System.in);
+    s=sc.nextInt();
+    }
+}
