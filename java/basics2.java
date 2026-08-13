22a6a7d2f501235b65ca14dc45733a7c304f3147
Kusum <kusumkumaryendredla@gmail.com>
2026-08-13 23:59:59 +0530

    ad

--- java/basics2.java
+++ java/basics2.java
@@ -9,6 +9,8 @@ public static void main(String[] args) {
     StringBuilder sb=new StringBuilder();
     sb.append("Hello");
     System.out.println(sb);
+    sb.append(" World");
+    System.out.println(sb);
 
 }
 }
