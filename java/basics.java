cc4c795ba28774742c389e4583967380ad5bec13
Kusum <kusumkumaryendredla@gmail.com>
2026-08-13 23:58:49 +0530

    add

--- java/basics.java
+++ java/basics.java
@@ -1,10 +1,13 @@
-import java.util.Scanner;
+import java.util.*;
 
 class basics {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         int a ;
     Scanner sc=new Scanner(System.in);
-    s=sc.nextInt();
+    a=sc.nextInt();
+    String  s=new String();
+    s=sc.nextLine();
+    System.out.println(a);
+    System.out.println(s);
     }
-}
---
+++ java/basics2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class basics2 {
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    String s=sc.nextLine();
+    System.out.println(a);
+    System.out.println(s);
+}
+}
