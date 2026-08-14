ca679b28a8fc0131d6cd60e0e71be30dcffdfa5d
Kusum <kusumkumaryendredla@gmail.com>
2026-08-14 23:45:16 +0530

    add

--- Python/basics2.py
+++ Python/basics2.py
@@ -11,3 +11,10 @@ def happy_number(num):
         num = sum(int(digit) ** 2 for digit in str(num))
     return num == 1 
 print(happy)
+
+
+#area of circles overlapping    
+
+a1 = float(input("Enter the radius of the first circle: "))
+a2 = float(input("Enter the radius of the second circle: "))
+a3 = float(input("Enter the distance between the centers of the circles: "))
